533e841500d523319dd8d6a88b949d10ddc8d7a1
Adrian Goins <oskapt@users.noreply.github.com>
2019-02-17 22:28:34 -0300

    copy default configuration
    if someone mounts an empty volume on `/mqtt/config` we want to populate it with our default configuration.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+if [[ ! -f /mqtt/config/mosquitto.conf ]]; then
+    # copy default config
+    echo "Copying default configuration to /mqtt/config"
+    cp -R /usr/share/mosquitto/config/* /mqtt/config/
+fi
 
 exec "$@"
