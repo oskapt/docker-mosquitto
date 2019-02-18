1622003bff2d883f47244a96a5d70a720f247773
Adrian Goins <oskapt@users.noreply.github.com>
2019-02-17 22:39:14 -0300

    accept alternative arguments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,9 @@ if [[ ! -f /mqtt/config/mosquitto.conf ]]; then
     cp -R /usr/share/mosquitto/config/* /mqtt/config/
 fi
 
+# Passing arguments
+if [[ "${1:0:1}" = '-' ]]; then
+  set -- /usr/sbin/mosquitto "$@"
+fi
+
 exec "$@"
